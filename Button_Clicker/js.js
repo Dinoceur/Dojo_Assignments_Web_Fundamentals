f721898b621d0328dc6b0dc82f9bf7c6e62a3292
Augustin Kim <austinkim14@gmail.com>
2021-11-16 10:28:03 -0800

    initial

---
+++ Button_Clicker/js.js
@@ -0,0 +1,16 @@
+function alert(element) {
+    element.innerText = "Logout";
+}
+
+function alert4(element) {
+    element.innerText = "";
+}
+
+function alert2(element) {
+    element.innerText = "14 likes";
+}
+
+
+function alert3(element) {
+    element.innerText = "38 likes";
+}
